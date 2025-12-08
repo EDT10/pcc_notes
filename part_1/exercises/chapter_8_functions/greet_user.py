86f4d9ce61adb85f3e6a2fd83fad6da41aa85130
Emmanuel Wright <19850320+EDT10@users.noreply.github.com>
2025-12-08 08:41:24 -0500

    Created chapter 8 folder and worked on exercise 8.1

---
+++ part_1/exercises/chapter_8_functions/greet_user.py
@@ -0,0 +1,5 @@
+def greet_user(username):
+    """Displaying a simple greeting."""
+    print(f"Hello, {username.title()}")
+
+greet_user("jesse")
---
+++ part_1/exercises/chapter_8_functions/message_8_1.py
@@ -0,0 +1,11 @@
+"""
+Write a function called display_message() that prints one sentence telling 
+everyone what you are learning about in this chapter. 
+Call the function, and make sure the message displays correctly.
+"""
+
+def message():
+    """Displays what is being learned in chapter 8"""
+    print("I am learning about functions in chapter 8.")
+
+message()
