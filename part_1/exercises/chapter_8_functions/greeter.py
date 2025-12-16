c94162441121f91acbcaa11a06f79404faca3e52
Emmanuel Wright <19850320+EDT10@users.noreply.github.com>
2025-12-16 08:08:20 -0500

    Added more notes about functions working with dictionaries and while loop

---
+++ part_1/exercises/chapter_8_functions/greeter.py
@@ -0,0 +1,23 @@
+def get_formatted_name(first_name, last_name, middle_name=""):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {middle_name} {last_name}"
+
+    return full_name.title()
+
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    f_name = input("First name: ")
+    if f_name == "q":
+        break
+
+    l_name = input("Last name: ")
+    if l_name == "q":
+        break
+
+    # m_name = input("Middle name: ")
+
+    formatted_name = get_formatted_name(f_name, l_name)
+
+    print(f"\nHello, {formatted_name}!")
+
