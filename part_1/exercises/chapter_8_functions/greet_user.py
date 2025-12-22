a3aedf8e5e44e19242e222f88f42596ac1ca3fc6
Emmanuel Wright <19850320+EDT10@users.noreply.github.com>
2025-12-21 19:57:29 -0500

    More notes and examples working with functions and lists.

--- part_1/exercises/chapter_8_functions/greet_user.py
+++ part_1/exercises/chapter_8_functions/greet_user.py
@@ -2,4 +2,18 @@ def greet_user(username):
     """Displaying a simple greeting."""
     print(f"Hello, {username.title()}")
 
-greet_user("jesse")
+#greet_user("jesse")
+
+
+# defining the function
+def greet_all_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+
+# creat list outside the function
+usernames = ["hanna", "ty", "margot"]
+
+#passing the list to the function so the items can be worked on individually.
+greet_all_users(usernames)
