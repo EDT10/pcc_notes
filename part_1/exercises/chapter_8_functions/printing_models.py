524b23eb0573aecdbe5bfd245c6992184696c6d8
Emmanuel Wright <19850320+EDT10@users.noreply.github.com>
2025-12-30 08:41:24 -0500

    finished chapter 8 and it's exercises

--- part_1/exercises/chapter_8_functions/printing_models.py
+++ part_1/exercises/chapter_8_functions/printing_models.py
@@ -17,25 +17,11 @@
 #     print(completed_model)
 
 
-def print_models(unprinted_designs, completed_models):
-    """
-    simulate printing each design, until noe are left.
-    Move each design to completed_models after printing.
-    """
-    while unprinted_designs:
-        current_design = unprinted_designs.pop()
-        print(f"Printing model: {current_design}")
-        completed_models.append(current_design)
+import printing_functions_8_15 as pf
 
-def show_completed_models(completed_models):
-    """Show all the models that were printed."""
-    print("\nThe following models have been printed:")
 
-    for completed_model in completed_models:
-        print(completed_model)
-
-unprinted_designs = ["phone case", "robot pendant", "dodecahedron"]
+designs_to_print = ["phone case", "robot pendant", "dodecahedron"]
 completed_models = []
 
-print_models(unprinted_designs, completed_models)
-show_completed_models(completed_models)
+pf.print_models(designs_to_print, completed_models)
+pf.show_completed_models(completed_models)
