d8db7bdf899043681f4d7421f778901415c25d1f
Emmanuel Wright <19850320+EDT10@users.noreply.github.com>
2026-03-05 08:34:29 -0500

    Adding modified files in my_test folder

--- part_1/exercises/chapter_11_testing_code/my_tests/test_cities_11_1.py
+++ part_1/exercises/chapter_11_testing_code/my_tests/test_cities_11_1.py
@@ -1,6 +1,11 @@
-from city_functions_11_1 import city_country
+from population_11_2 import city_country
 
 def test_city_country():
     """Does Santiago and Chile work?"""
     formatted_string = city_country("santiago", "chile")
     assert formatted_string == "Santiago, Chile"
+
+def test_city_country_population():
+    """Does Santiago, Chile and 5_000_000 work?"""
+    formatted_string = city_country("santiago", "chile", "5000000")
+    assert formatted_string == "Santiago, Chile - population 5000000"
